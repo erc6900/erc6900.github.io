b045ac8af68818a9ce196509012e393b75c1fc1a
Cody McCabe <cmccabe524@gmail.com>
2025-03-03 15:38:29 -0500

    update tailwind and deployment action

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,md,mdx,tsx,js,jsx}"],
+  content: [
+    "./docs/**/*.{html,md,mdx,tsx,js,jsx}",
+    "./src/**/*.{html,md,mdx,tsx,js,jsx}",
+    "./components/**/*.{html,md,mdx,tsx,js,jsx}",
+  ],
   theme: {
     extend: {},
   },
